45b79f3517cd3c3d96792e0270667ffb3ccf8853
ajmalazeez01 <ajmalazeez776@gmail.com>
2023-08-12 12:15:50 +0530

    first

--- src/Config/AdminBaseApi.jsx
+++ src/Config/AdminBaseApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const USER_BASE_URL= `https://bandid.site/api/admin`
-// const USER_BASE_URL= `http://localhost:5000/api/admin`
+const USER_BASE_URL= `https://bandid.site/backend/admin`
+// const USER_BASE_URL= `http://localhost:5000/backend/admin`
 
 const AdminApi =axios.create({
     baseURL:USER_BASE_URL

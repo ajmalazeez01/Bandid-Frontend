7dc5cdc129c2ed57154a2277832e05310a1f5719
ajmalazeez01 <ajmalazeez776@gmail.com>
2023-08-12 20:02:58 +0530

    first

--- src/Config/BandBaseApi.jsx
+++ src/Config/BandBaseApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const USER_BASE_URL= `https://bandid.site/backend/band`
-const USER_BASE_URL= `http://localhost:5000/backend/band`
+const USER_BASE_URL= `https://bandid.site/backend/band`
+// const USER_BASE_URL= `http://localhost:5000/backend/band`
 
 const BandApi =axios.create({
     baseURL:USER_BASE_URL
